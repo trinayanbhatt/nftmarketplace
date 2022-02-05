bf796232ca6b1ff7a201423f22849bb348cc164e
Trinayan Bhatt <Bhatt_t@India.XoriantCorp.com>
2022-02-05 10:58:15 +0530

    added nftmarketplace contract and test script for testing the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,21 +2,21 @@ require("@nomiclabs/hardhat-waffle");
 const fs = require('fs')
 const privkey = fs.readFileSync(".secret").toString()
 
-const projectId = env.projectId;
+//const projectId = env.projectId;
 
 module.exports = {
   networks:{
     hardhat:{
       chainId: 1337
     },
-    mumbai:{
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts=[privkey]
-    },
-    mainnet:{
-      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-      accounts=[privkey]
-    }
+    // mumbai:{
+    //   url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+    //   accounts=[privkey]
+    // },
+    // mainnet:{
+    //   url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+    //   accounts=[privkey]
+    // }
   },
   solidity: "0.8.4",
 };
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,36 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await Market.deploy()
+    await nft.deployed()
+    const nftaddress = nft.address
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether')
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await nft.createToken('https://www.mytokenlocation.com')
+    await nft.createToken('https://www.mytokenlocation2.com')
+
+    await market.createMarektItem(nftaddress, 1, auctionPrice, {value: listingPrice})
+    await market.createMarektItem(nftaddress, 2, auctionPrice, {value: listingPrice})
+
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSale(nftaddress, 1, {value: auctionPrice})
+    
+    const items = await market.fetchMarketItems()
+
+    console.log('items:', items)
+
   });
 });
