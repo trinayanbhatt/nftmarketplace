62845351efbef6f2525c9bd4f394dcc1b99537cf
Trinayan Bhatt <Bhatt_t@India.XoriantCorp.com>
2022-02-07 11:50:05 +0530

    added front end code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
+/* pages/_app.js */
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">NFTs Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-green-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-green-500">
+              Sell NFT
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-green-500">
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/minter-dashboard">
+            <a className="mr-6 text-green-500">
+              Minter Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
